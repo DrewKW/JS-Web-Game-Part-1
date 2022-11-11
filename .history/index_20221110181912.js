0c6974a87c6480e44b8a5fc41deb707afb197da4
Andrew Wokabi <drewzoteschool@gmail.com>
2022-11-10 18:26:36 -0800

    Improved Game motions

--- .history/index_20221110181912.js
+++ .history/index_20221110181912.js
@@ -30,4 +30,15 @@ newImage('assets/well.png','500px','425px')
 newItem('assets/sword.png','500px','405px')
 newItem('assets/sheild.png','165px','185px')
 newItem('assets/staff.png','600px','100px')
-
+/*let greenCharacter = document.createElement('img')
+greenCharacter.src = 'assets/green-character.gif'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '100px'
+greenCharacter.style.bottom = '100px'
+document.body.append(greenCharacter)
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)*/
